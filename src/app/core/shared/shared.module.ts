ab8aad435f0e12e20bca044545862099032e2a32
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-11 14:46:36 -0400

    Layer: Core - Shared file (shared.module.ts) [11/12/2023]

---
+++ src/app/core/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }                         from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule }                     from '@angular/router';
+
+@NgModule({
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    declarations: [],
+    exports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    providers: []
+})
+
+export class SharedModule {
+    constructor() {  }
+}
