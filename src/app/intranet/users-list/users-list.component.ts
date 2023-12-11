d6627fdb995732d11ebfbfe393efc6c72f57d60a
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-11 16:43:03 -0400

    Fix - users-list.component.ts & Add <router-outlet> app.component.html [11/12/2023]

--- src/app/intranet/users-list/users-list.component.ts
+++ src/app/intranet/users-list/users-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
        selector: 'app-users-list',
-    templateUrl: './user-list.component.html',
+    templateUrl: './users-list.component.html',
       styleUrls:['./users-list.component.scss']
 })
 
