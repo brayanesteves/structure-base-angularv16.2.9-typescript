3d970a6d5f68311bef10d27836548969d633c118
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-11 16:12:34 -0400

    Layer [intranet] - Users (Module, Component, Routing)  [11/12/2023]

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,7 +2,7 @@ import { NgModule }            from '@angular/core';
 import { SharedModule }        from '../core/shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent }     from './public.component';
-import { LoginComponent } from './auth/login/containers/login/login.component';
+import { LoginComponent }      from './auth/login/containers/login/login.component';
 
 @NgModule({
     imports: [
